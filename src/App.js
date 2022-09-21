7d9ea9ac9e4c83303aaab323d0a187bf46fc4342
Md-Hafizur-Rahman <hafizur15-1979@diu.edu.bd>
2022-09-21 22:11:19 +0600

    lists and keys

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Page from './component/Showhide';
 import Toggle from './component/helo';
 import LoginControl from './component/Logincontrol';
 import Greeting from './component/Greeting';
+import Blog from './component/practise';
 
 import React,{Component} from 'react';
 
 class App extends Component{
   render(){
+    const numbers = [1, 2, 3, 4, 5];
+    const posts = [
+      {id: 1, title: 'Title number 1', content: 'the content of the title 1'},
+      {id: 2, title: 'title number 2', content: 'the content of the title 2'}
+    ];
     return(
       <div className='App'>
-        <Welcome name='hafizur' lastname='rahman'>
-          <p>welcom to the react js.</p>
+        <Welcome name='hafizur' lastname='rahman' numbers={numbers}>
+          <p>welcom to the react js</p>
           <button>Action</button>
         </Welcome>
         <Toggle/>
@@ -21,8 +27,10 @@ class App extends Component{
         <Page/>
         <Greeting isLoggedIn={false} />
         <LoginControl/>
+        <Blog posts={posts} />
       </div>
     );
+   
   }
 }
 export default App;
--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -1,12 +1,17 @@
 import React from "react";
 const Welcome=props=>{
-  return(
-    <div>
-      
-      <h1>Name: {props.name} - {props.lastname}</h1>
-      {props.children}
-    </div>
+
+  const numbers = props.numbers;
+  const listItems = numbers.map((number,index) => 
+    <li key={index}>{number}</li>
   );
-  
+  return (
+    <div>
+      <h1>Helo {props.name}-{props.lastname}</h1>
+      <ul>{listItems}</ul>
+    </div>
+    
+  );
 }
+
 export default Welcome;
